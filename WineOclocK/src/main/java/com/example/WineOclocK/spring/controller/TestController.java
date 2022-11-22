ffe6d528a02730c0115d01b8421c085e2ee1625b
bnfkim <kgr3819@gmail.com>
2022-11-23 00:53:28 +0900

    [fix] test Contorlloer

--- WineOclocK/src/main/java/com/example/WineOclocK/spring/controller/TestController.java
+++ WineOclocK/src/main/java/com/example/WineOclocK/spring/controller/TestController.java
@@ -1,14 +1,32 @@
 package com.example.WineOclocK.spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
     @PostMapping("/test")
-    public String test() {
-        return "<h1> test 통과 </h1>";
+    public String test1() {
+        return "<h1> post test 통과 </h1>";
+    }
+
+    @GetMapping("/test")
+    public String test2() {
+        return "<h1> get test 통과 </h1>";
+    }
+
+    @DeleteMapping("/test")
+    public String test3() {
+        return "<h1> delete test 통과 </h1>";
+    }
+
+    @PutMapping("/test")
+    public String test4() {
+        return "<h1> put test 통과 </h1>";
+    }
+
+    @PatchMapping("/test")
+    public String test5() {
+        return "<h1> patch test 통과 </h1>";
     }
 
     @GetMapping("/hello")
