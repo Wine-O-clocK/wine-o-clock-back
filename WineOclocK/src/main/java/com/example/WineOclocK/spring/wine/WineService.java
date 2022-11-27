5095f3e460c38d6584a09b7389ec3de22a6c8b80
bnfkim <kgr3819@gmail.com>
2022-11-27 21:36:18 +0900

    [feat] based server upload

--- WineOclocK/src/main/java/com/example/WineOclocK/spring/wine/WineService.java
+++ WineOclocK/src/main/java/com/example/WineOclocK/spring/wine/WineService.java
@@ -1,34 +1,34 @@
-package com.example.WineOclocK.spring.wine;
-
-import org.springframework.stereotype.Service;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-@Service
-public class WineService {
-
-    // data.json 파일을 전부 읽어서 String 으로 반환
-    public String getCrawling() throws IOException {
-        return readLines("data.json");
-    }
-
-    // recent_data.json 파일을 전부 읽어서 String 으로 반환
-    public String getCrawlingRecent() throws IOException {
-        return readLines("recent_data.json");
-    }
-
-    // fileName 의 파일 내용을 전부 String 으로 읽어서 반환
-    private String readLines(String fileName) {
-        return new BufferedReader(
-                new InputStreamReader(
-                        // 지정한 fileName 을 resources 디렉토리에서 읽어 옴
-                        Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream(fileName))
-                        //this.getClass().getClassLoader().getResourceAsStream(fileName)
-                )
-        ).lines().collect(Collectors.joining());
-    }
-}
+//package com.example.WineOclocK.spring.wine;
+//
+//import org.springframework.stereotype.Service;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Objects;
+//import java.util.stream.Collectors;
+//
+//@Service
+//public class WineService {
+//
+//    // data.json 파일을 전부 읽어서 String 으로 반환
+//    public String getCrawling() throws IOException {
+//        return readLines("data.json");
+//    }
+//
+//    // recent_data.json 파일을 전부 읽어서 String 으로 반환
+//    public String getCrawlingRecent() throws IOException {
+//        return readLines("recent_data.json");
+//    }
+//
+//    // fileName 의 파일 내용을 전부 String 으로 읽어서 반환
+//    private String readLines(String fileName) {
+//        return new BufferedReader(
+//                new InputStreamReader(
+//                        // 지정한 fileName 을 resources 디렉토리에서 읽어 옴
+//                        Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream(fileName))
+//                        //this.getClass().getClassLoader().getResourceAsStream(fileName)
+//                )
+//        ).lines().collect(Collectors.joining());
+//    }
+//}
--- WineOclocK/src/main/java/com/example/WineOclocK/spring/wine/crawling/CrawlingRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.WineOclocK.spring.wine.crawling;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrawlingRepository extends JpaRepository<Crawling, Long> {
-}
