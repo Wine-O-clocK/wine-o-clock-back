eed234a991efe105d04cc7ca967f606a7f469b49
bnfkim <kgr3819@gmail.com>
2022-12-04 11:57:24 +0900

    [fix] crawling file

--- WineOclocK/src/main/java/com/example/WineOclocK/spring/crawling/CrawlingRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.example.WineOclocK.spring.crawling;
-//
-//import com.example.WineOclocK.spring.domain.entity.Crawling;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface CrawlingRepository extends JpaRepository<Crawling, Long> {
-//}
