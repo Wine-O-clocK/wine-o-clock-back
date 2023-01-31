82566d5a13c9c9d81e7c23a379ce870ad2969aa2
bnfkim <kgr3819@gmail.com>
2023-01-31 15:41:06 +0900

    [feat] DetailResDto

---
+++ WineOclocK/src/main/java/com/example/WineOclocK/spring/wine/dto/DetailResDto.java
@@ -0,0 +1,13 @@
+package com.example.WineOclocK.spring.wine.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class DetailResDto {
+
+    private Long userId;
+    private boolean like;
+
+}
